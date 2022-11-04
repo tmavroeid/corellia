fa7a7592a47249b74ad7d053b67d25ce5cab6d52
teo <tmavroeidakos@gmail.com>
2022-11-04 16:03:42 +0200

    feat: update with bin and minor changes to logic.

--- README.md
+++ README.md
@@ -1,28 +1,43 @@
-# matic-funder
-Matic funding script from Alchemy faucet.
-
-##### Table of Contents  
-- [Introduction](#introduction)
-- [Prerequisites](#prerequisites)  
-- [Usage](#usage)
-
-## Introduction
-This project contains a script which by scraping the mumbai faucet of Alchemy gets 5 Matic and then distributes 1 Matic to the 4 addresses used in staging and production and keeps 1 Matic in the FUNDED_ADDRESS for future use.
+# funmb
+A CLI tool for funding an address with Matic Testnet Tokens using the Alchemy faucet.
+![release passing](https://github.com/tmavroeid/funmb/actions/workflows/release.yml/badge.svg)
+![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/tmavroeid/funmb)
 
 
-## Prerequisites
+##### Table of Contents 
+* [Installation](#installation)
+* [Build](#build)
+* [Introduction](#introduction)
+* [Prerequisites](#prerequisites)  
+* [Usage](#usage)
 
-Please make sure:
-1) Install the project's dependencies with the following command:
+## <a name="installation">Installation</a>
 
-```
+Install the package with yarn or npm:
+
+```bash
+yarn install
 npm install
 ```
 
-2) To initialize a **.env** file following the environmental variables in **.env.template**. 
+## <a name="build">Build</a>
+
+Build the package with yarn or npm:
+
+```bash
+yarn build
+npm build
+```
+
+## <a name="introduction">Introduction</a>
+This project contains a script which by scraping the mumbai faucet of Alchemy gets 5 Matic and then distributes 1 Matic to the 4 addresses used in staging and production and keeps 1 Matic in the FUNDED_ADDRESS for future use.
+
+
+## <a name="prerequisites">Prerequisites</a>
+
+Please make sure to initialize an **.env** file following the environmental variables in **.env.template**. 
 
 - An account should be created in [Alchemy](https://www.alchemy.com) in order to use it in the faucet and get 5 Matic/day instead of 1 Matic/day. The funding from the faucet can take place once every 24 hours.
-- An account should be created in Infura in order to get an API Key and use a node for the purpose of interacting with the Polygon PoS.
 
 
 ## Usage
@@ -31,3 +46,9 @@ To initiate the daily funding:
 ```
 npm run funding
 ```
+
+## <a name="license">License</a>
+
+This library is released under MIT.
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/tmavroeid)
--- funder.js
+++ funder.js
@@ -1,8 +1,12 @@
+#! /usr/bin/env node
+const { program } = require('commander')
 require('dotenv').config()
 const logger = require('./common/logger')
 const puppeteer = require('puppeteer')
 const Web3 = require('web3')
-const web3 = new Web3(`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`)
+const web3 = process.env.ALCHEMY_API_KEY ? 
+             new Web3(`https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`) :
+             new Web3('https://rpc-mumbai.matic.today')
 
 const addresses = process.env.ADDRESSES.split(',').map(item => item.trim())
 const fundedAddress = web3.utils.toChecksumAddress(process.env.FUNDED_ADDRESS)
@@ -30,13 +34,20 @@ async function getMatic () {
     await page.type('#gatsby-focus-wrapper > div > div.css-zkbqul > div.css-r17iry > div > div.css-1c73mnp > form > label.css-1ca7lze > input', email)
     await page.type('#gatsby-focus-wrapper > div > div.css-zkbqul > div.css-r17iry > div > div.css-1c73mnp > form > label.css-2wrca4 > input', pass)
     await Promise.all([
+      page.click('#gatsby-focus-wrapper > div > div.css-zkbqul > div.css-r17iry > div > div.css-1c73mnp > form > button'),
       page.waitForNavigation(),
-      page.click('#gatsby-focus-wrapper > div > div.css-zkbqul > div.css-r17iry > div > div.css-1c73mnp > form > button')
-    ])
+      ]);
     await page.type('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-9.col-sm-12 > input', fundedAddress)
     const element = await page.waitForSelector('#root > div.alchemy-app > div.alchemy-app-content-container > div:nth-child(2) > div.row > div > span > div:nth-child(1) > div.col-md-3.col-sm-12 > button')
     await element.evaluate(sendMatic => sendMatic.click())
+    const success = await page.type('#root > div.basket > div > div > h3')
+    const failure = await page.type('#root > div.alchemy-app > div.alchemy-app-content-container > div.alchemy-faucet-title-component.container > div.faucet-banner-container.show > div')
     browser.close()
+    if (failure){
+      throw new Error('No Funding Took Place')
+    }else{
+      return true
+    }
   } catch (err) {
     return err
   }
@@ -47,13 +58,28 @@ function delay (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function retryGetBalance (fundedAddress, retriesLeft = 10) {
+function getBalance (fundedAddress) {
   return web3.eth.getBalance(fundedAddress)
     .then((balance) => {
-      if (web3.utils.fromWei(String(balance)) > 5) {
+      return web3.utils.fromWei(String(balance))
+    })
+    .catch((error) => {
+      return error
+    })
+}
+
+function retryGetBalance (fundedAddress, balanceBefore, retriesLeft = 10) {
+  return web3.eth.getBalance(fundedAddress)
+    .then((balance) => {
+      if (retriesLeft === 1) {
+        info('MAXIMUM RETRIES EXCEEDED')
+        return new Error(`MAXIMUM RETRIES EXCEEDED FOR GET BALANCE FOR ${fundedAddress}`)
+      }
+      else if(balanceBefore>=web3.utils.fromWei(String(balance))){
+        info(`RETRYING GET BALANCE FOR ${fundedAddress}`)
+        return delay(10000).then(() => { return retryGetBalance(fundedAddress, balanceBefore, retriesLeft - 1) })
+      }else{
         return web3.utils.fromWei(String(balance))
-      } else {
-        throw new Error('Balance is not updated')
       }
     })
     .catch((error) => {
@@ -61,44 +87,50 @@ function retryGetBalance (fundedAddress, retriesLeft = 10) {
         info('MAXIMUM RETRIES EXCEEDED')
         return Promise.reject(error)
       }
-
       info(`RETRYING GET BALANCE FOR ${fundedAddress}`)
-      return delay(10000).then(() => { return retryGetBalance(fundedAddress, retriesLeft - 1) })
+      return delay(10000).then(() => { return retryGetBalance(fundedAddress, balanceBefore, retriesLeft - 1) })
     })
 }
 
-async function distributeMatic () {
+
+async function distributeMatic (fundedAmount = 2) {
   const receipts = []
   const gasPrice = await web3.eth.getGasPrice()
-  return retryGetBalance(fundedAddress)
-    .then(async (balance) => {
-      for (const address of addresses) {
-        const transaction = {
-          from: fundedAddress,
-          to: web3.utils.toChecksumAddress(address),
-          data: '0x',
-          value: web3.utils.toWei('1'),
-          gasLimit: web3.utils.toHex('21000'),
-          gasPrice: web3.utils.toHex((gasPrice))
-        }
-        const signedTx = await web3.eth.accounts.signTransaction(
-          transaction,
-          fundedAddressPrivateKey
-        )
-        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-        receipts.push(receipt.transactionHash)
-      }
-      return receipts
-    })
-    .catch((err) => {
-      return err
-    })
+  const valueToDistribute = fundedAmount/adresses.length
+  for (const address of addresses) {
+    const transaction = {
+      from: fundedAddress,
+      to: web3.utils.toChecksumAddress(address),
+      data: '0x',
+      value: web3.utils.toWei(String(valueToDistribute)),
+      gasLimit: web3.utils.toHex('21000'),
+      gasPrice: web3.utils.toHex((gasPrice))
+    }
+    const signedTx = await web3.eth.accounts.signTransaction(
+      transaction,
+      fundedAddressPrivateKey
+    )
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+    receipts.push(receipt.transactionHash)
+  }
+  return receipts
 }
 
 async function main () {
   try {
+    let balanceBefore = await getBalance(fundedAddress)
+    info(`BALANCE BEFORE IS ${balanceBefore}`)
     await getMatic()
-    const receipts = await distributeMatic()
+    let balanceAfter = await retryGetBalance(fundedAddress, balanceBefore)
+    info(`BALANCE AFTER IS ${balanceAfter}`)
+    let receipts = []
+    let fundedAmount = balanceAfter-balanceBefore
+    if ( fundedAmount === 2) {
+      receipts = await distributeMatic(balanceAfter)
+    } else {
+      throw new Error('Balance is not updated')
+    }
+    receipts = await distributeMatic(fundedAmount)
     Array.isArray(receipts) ? info(receipts) : info('NO FUNDING TOOK PLACE')
   } catch (err) {
     error('MATIC FUNDER', err)
