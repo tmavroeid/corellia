0f01b930276925cde916c26dd290fa25ed30c1ed
teo <tmavroeidakos@gmail.com>
2022-11-22 00:01:19 +0200

    feat: update funder.

--- funder.js
+++ funder.js
@@ -8,7 +8,6 @@ const log = console.log
 const info = (msg) => log(chalk.green.bold(`${msg}`))
 const working = (msg) => log(chalk.yellow.bold(`${msg}`))
 const error = (message) => log(chalk.red.bold(`${message}`))
-
 const validateEmail = (email) => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
